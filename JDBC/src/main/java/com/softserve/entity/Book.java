cdf70d61b0478177252b14c61d27889ccdec324c
Oleksandr <ssilich@bitbucket.org>
2019-04-10 23:21:26 +0300

    add dao package

---
+++ JDBC/src/main/java/com/softserve/entity/Book.java
@@ -0,0 +1,52 @@
+package com.softserve.entity;
+
+import java.io.Serializable;
+
+public class Book implements Serializable {
+    private Integer id;
+    private String name;
+    private String releaseDate;
+    private boolean available;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(String releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+    public boolean isAvailable() {
+        return available;
+    }
+
+    public void setAvailable(boolean available) {
+        this.available = available;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", releaseDate='" + releaseDate + '\'' +
+                ", available=" + available +
+                '}';
+    }
+}
